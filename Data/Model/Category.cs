0ed92f65b61bf5633d5c5c9892d5ab7acdfa4eb4
Rene <kollarits.rene@gmx.at>
2021-11-29 19:12:09 +0100

    change model

---
+++ Data/Model/Category.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pery.Data.Model
+{
+
+    public class Category
+    {
+        private Guid id;
+        private string imageSrc;
+        private string productName;
+        private string description;
+        private bool active;
+
+        public bool Active
+        {
+            get
+            { return active; }
+            set
+            {
+                active = value;
+            }
+        }
+
+        public Guid Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string ImagesSrc
+        {
+            get
+            {
+                return imageSrc;
+            }
+            set
+            {
+                imageSrc = value;
+            }
+        }
+    }
+}
---
+++ Data/Model/Product.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pery.Data.Model
+{
+    public class Product
+    {
+        private int id;
+        private List<Guid> categoryIds;
+        private string productName;
+        private int collectionid;
+        private string description;
+        private string imageSrc;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int CollectionId
+        {
+            get { return collectionid; }
+            set { collectionid = value; }
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string ImagesSrc
+        {
+            get
+            {
+                return imageSrc;
+            }
+            set
+            {
+                imageSrc = value;
+            }
+        }
+
+        public List<Guid> CategoryIds
+        {
+            get { return categoryIds; }
+            set { categoryIds = value; }
+        }
+    }
+}
